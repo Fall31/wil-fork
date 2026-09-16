85947bf4c74a17a8d170f59dd4c1ef5c6d81b190
Fall31 <lazomorenowilsonlazo@gmail.com>
2026-09-16 19:29:34 -0400

    wil:init:inicio del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="pagina">
+      <header>
+        <h1>Repositorios y Forks</h1>
+      </header>
+
+      <main>
+        <p>
+          Esta es la pagina para la organizacion
+        </p>
+      </main>
+
+      <footer>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
